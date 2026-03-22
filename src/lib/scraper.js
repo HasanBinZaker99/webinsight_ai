95a56085046666a1ca6e3eebed4b0c1810c8f977
Md Hasan Zaker <mdhasanzaker@mds-macbook-pro.home>
2026-03-22 04:44:50 -0230

    add website scraping utility with axios and cheerio

---
+++ src/lib/scraper.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export async function fetchWebsiteData(url) {
+  const response = await axios.get(url, {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+    },
+    timeout: 10000,
+  });
+  const html = response.data;
+  const page = cheerio.load(html);
+
+  page("script").remove();
+  page("style").remove();
+  page("noscript").remove();
+
+  const text = page("body").text().replace(/\s+/g, " ").trim(); /*
+  \s+ means Find one or more whitespace characters in a row.
+  g means: “global” This tells JavaScript: Do this for the whole
+   string, not just the first match. Without g, only the first extra space group would be replaced. With g, all extra spaces everywhere are cleaned.”
+  */
+  const shortText = text;
+
+  return {
+    text: shortText,
+  };
+}
